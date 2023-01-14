89ff77ba9c1181f4b38999dd57b5dd9471540240
iaror14 <isha.arora561@gmail.com>
2023-01-14 15:44:13 +0530

    serverURL

--- app.js
+++ app.js
@@ -6,3 +6,8 @@ console.log(btnTranslateID);
 
 var outputText = document.querySelector("#output-area");
 console.log(outputText);
+
+var serverURL = "https://api.funtranslations.com/translate/pig-latin.json";
+function finalServer(text) {
+  return serverURL + "?" + "text=" + text;
+}
