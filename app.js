6740d1cff1088d35a324d44ad5bc68a36c34de26
iaror14 <isha.arora561@gmail.com>
2023-01-14 15:29:17 +0530

    queryselector

---
+++ app.js
@@ -0,0 +1,8 @@
+var txtAreaInput = document.querySelector("#txt-area");
+console.log(txtAreaInput);
+
+var btnTranslateID = document.querySelector("#btn-id");
+console.log(btnTranslateID);
+
+var outputText = document.querySelector("#output-area");
+console.log(outputText);
