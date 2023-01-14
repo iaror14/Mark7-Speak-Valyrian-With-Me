0540ab12b55556eef072cb03b298bb6e1e073553
iaror14 <isha.arora561@gmail.com>
2023-01-14 15:45:11 +0530

    click Handler + fetch

--- app.js
+++ app.js
@@ -11,3 +11,16 @@ var serverURL = "https://api.funtranslations.com/translate/pig-latin.json";
 function finalServer(text) {
   return serverURL + "?" + "text=" + text;
 }
+
+function clickHandler() {
+  console.log("clicked");
+  var inputText = txtAreaInput.value;
+
+  fetch(getTranslation(inputText))
+    .then((response) => response.json())
+    .then((json) => {
+      var translateText = json.contents.translated;
+      outputText.innerText = translateText;
+    });
+}
+btnTranslateID.addEventListener("click", clickHandler);
